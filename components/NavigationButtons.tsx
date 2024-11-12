37d53976cc74ecb01cf5bd01c2021db4a8e9c600
Charaf Salmi <charafsalmi@users.noreply.github.com>
2024-11-12 14:28:29 +0100

    Fix for previous/next controls

--- components/NavigationButtons.tsx
+++ components/NavigationButtons.tsx
@@ -4,27 +4,31 @@ import { NavigationButtonsProps } from "../interfaces/NavigationButtonsProps";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight, Home } from "lucide-react";
 
-const NavigationButtons = ({ id, maxId }: NavigationButtonsProps) => (
-  <div className=" items-center w-full flex justify-between">
-    <Link href={`/pokemon/${id - 1}`}>
-      <Button disabled={id <= 1}>
-        <ChevronLeft />
-        Previous
-      </Button>
-    </Link>
-    <Link href={`/`}>
-      <Button>
-        <Home />
-        Index
-      </Button>
-    </Link>
-    <Link href={`/pokemon/${id + 1}`}>
-      <Button disabled={id >= maxId}>
-        Next
-        <ChevronRight />
-      </Button>
-    </Link>
-  </div>
-);
+const NavigationButtons = ({ id, maxId }: NavigationButtonsProps) => {
+  const previousDisabled = id <= 1;
+  const nextDisabled = id >= maxId;
+  return (
+    <div className=" items-center w-full flex justify-between">
+      <Link href={previousDisabled ? '/' : `/pokemon/${id - 1}`}>
+        <Button disabled={id <= 1}>
+          <ChevronLeft />
+          Previous
+        </Button>
+      </Link>
+      <Link href={`/`}>
+        <Button>
+          <Home />
+          Index
+        </Button>
+      </Link>
+      <Link href={nextDisabled ? '/' : `/pokemon/${id + 1}`}>
+        <Button disabled={id >= maxId}>
+          Next
+          <ChevronRight />
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
 export default NavigationButtons;
--- components/PaginationControls.tsx
+++ components/PaginationControls.tsx
@@ -2,25 +2,28 @@ import React from "react";
 import { PaginationControlsProps } from "../interfaces/PaginationControlsProps";
 import { Button } from "@/components/ui/button";
 
-
-const PaginationControls = ({ currentPage, totalPages, onPageChange }: PaginationControlsProps) => (
-  <div className="flex w-full items-center space-x-2 justify-end">
-    <Button size="sm" onClick={() => onPageChange(1)} disabled={currentPage === 1}>
-      First
-    </Button>
-    <Button size="sm" onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
-      Previous
-    </Button>
-    <span>
-      {currentPage} / {totalPages}
-    </span>
-    <Button size="sm" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
-      Next
-    </Button>
-    <Button size="sm" onClick={() => onPageChange(totalPages)} disabled={currentPage === totalPages}>
-      Last
-    </Button>
-  </div>
-);
+const PaginationControls = ({ currentPage, totalPages, onPageChange }: PaginationControlsProps) => {
+  const previousDisabled = currentPage === 1;
+  const nextDisabled = currentPage === totalPages;
+  return (
+    <div className="flex w-full items-center space-x-2 justify-end">
+      <Button size="sm" onClick={() => onPageChange(1)} disabled={previousDisabled}>
+        First
+      </Button>
+      <Button size="sm" onClick={() => !previousDisabled && onPageChange(currentPage - 1)} disabled={previousDisabled}>
+        Previous
+      </Button>
+      <span>
+        {currentPage} / {totalPages}
+      </span>
+      <Button size="sm" onClick={() => !nextDisabled && onPageChange(currentPage + 1)} disabled={nextDisabled}>
+        Next
+      </Button>
+      <Button size="sm" onClick={() => onPageChange(totalPages)} disabled={nextDisabled}>
+        Last
+      </Button>
+    </div>
+  );
+};
 
 export default PaginationControls;
